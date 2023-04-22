75a054764cbb90230a3502d67993762da88d469f
nobushi95 <nobushi1121@gmail.com>
2023-04-23 01:33:39 +0900

    コードビハインドの実装追加

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+using Microsoft.UI.Text;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Documents;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -28,6 +30,24 @@ namespace TextBlockInlinesWinUI3
             // ウィンドウサイズを変更不能にする
             var presenter = appWindow.Presenter as OverlappedPresenter;
             presenter.IsResizable = false;
+
+            // コードビハインドでInlines追加
+            InitMyTextBlock();
+        }
+
+        private void InitMyTextBlock()
+        {
+            myTexxtBlock.Inlines.Clear();
+            var default1 = new Run() { Text = "デフォルトのサイズ2" };
+            var default2 = new Run() { Text = "デフォルトのサイズ2" };
+            var large = new Run() { Text = "大きいサイズ", FontSize = 40 };
+            var bold = new Run() { Text = "太字", FontWeight = FontWeights.Bold };
+            var small = new Run() { Text = "小さいサイズ", FontSize = 10 };
+            myTexxtBlock.Inlines.Add(default1);
+            myTexxtBlock.Inlines.Add(default2);
+            myTexxtBlock.Inlines.Add(large);
+            myTexxtBlock.Inlines.Add(bold);
+            myTexxtBlock.Inlines.Add(small);
         }
     }
 }
