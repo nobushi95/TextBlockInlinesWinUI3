0ef3984d2bc8fdd9355de3b3fac4c2522042901f
nobushi95 <nobushi1121@gmail.com>
2023-04-23 12:56:32 +0900

    MainWindowのコントロールをMainPageに分ける

---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Text;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Documents;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace TextBlockInlinesWinUI3.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            // コードビハインドでInlines追加
+            InitMyTextBlock();
+        }
+
+        private void InitMyTextBlock()
+        {
+            myTexxtBlock.Inlines.Clear();
+            var default1 = new Run() { Text = "デフォルトのサイズ2" };
+            var default2 = new Run() { Text = "デフォルトのサイズ2" };
+            var large = new Run() { Text = "大きいサイズ", FontSize = 40 };
+            var bold = new Run() { Text = "太字", FontWeight = FontWeights.Bold };
+            var small = new Run() { Text = "小さいサイズ", FontSize = 10 };
+            myTexxtBlock.Inlines.Add(default1);
+            myTexxtBlock.Inlines.Add(default2);
+            myTexxtBlock.Inlines.Add(large);
+            myTexxtBlock.Inlines.Add(bold);
+            myTexxtBlock.Inlines.Add(small);
+        }
+    }
+}
